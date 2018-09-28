d5874f60d8c8badc95ee365bd5e4c5fd6c9b0496
splm <splm_lis@163.com>
2018-09-28 11:53:43 +0800

    commit My merge info.

--- src/me/splm/test/Main.java
+++ src/me/splm/test/Main.java
@@ -7,5 +7,7 @@ public class Main {
     public static void main(String[] args) {
         File file1=new File("");
         System.out.print("Has fixed the problems belong to Issue101.");
+        boolean isRename = file1.renameTo(new File(""));
+        System.out.print("This is my new function.");
     }
 }
