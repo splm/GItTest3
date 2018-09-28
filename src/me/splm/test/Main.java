0d713749a375f25cdf489232bb29a1ecdd8061f9
splm <splm_lis@163.com>
2018-09-28 10:35:51 +0800

    change text of first line.

--- src/me/splm/test/Main.java
+++ src/me/splm/test/Main.java
@@ -1,8 +1,10 @@
 package me.splm.test;
 
+import java.io.File;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.print("Test word!");
+        File file1=new File("");
     }
 }
